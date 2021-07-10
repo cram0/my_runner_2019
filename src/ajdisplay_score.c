0a1a2b021affe578b1ccb96e4d4d0103d2cc2a35
cram0 <julien.augugliaro@epitech.eu>
2021-07-10 22:37:54 +0200

    Update: Score now updates normally

--- src/ajdisplay_score.c
+++ src/ajdisplay_score.c
@@ -9,5 +9,8 @@
 
 void ajd_score(running_scene *_running_scene)
 {
+    char score_str[100] = {'\0'};
+    sprintf(score_str, "Score \n %d", _running_scene->score);
+    sfText_setString(_running_scene->score_text, score_str);
     sfRenderWindow_drawText(_running_scene->window, _running_scene->score_text, NULL);
 }
--- src/ajupdate_score.c
+++ src/ajupdate_score.c
@@ -12,9 +12,6 @@ void aju_score(running_scene *_running_scene)
     if (!_running_scene->clock) { _running_scene->clock = sfClock_create(); }
     if (sfTime_asMilliseconds(sfClock_getElapsedTime(_running_scene->clock)) >= 750) {
         _running_scene->score++;
-        char score_str[100] = {'\0'};
-        sprintf(score_str, "Score \n %d", _running_scene->score);
-        sfText_setString(_running_scene->score_text, score_str);
         sfClock_restart(_running_scene->clock);
     }
 }
